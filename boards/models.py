5496547a4de9d7dec1d12d2084073170d985aace
수현 <id_suhyun@naver.com>
2023-09-03 13:08:50 +0900

    feat: First commit

--- boards/models.py
+++ boards/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from common.models import Commonmodel
 
-# Create your models here.
+
+class Board(Commonmodel):
+    pageId = models.AutoField(primary_key=True)
+
+    title = models.CharField(
+        max_length=100,
+        verbose_name="제목",
+    )
+    contents = models.TextField(
+        max_length=500,
+        verbose_name="내용",
+    )
+    parent_page = models.ForeignKey(
+        "self",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name="상위 페이지",
+    )
+
+    def __str__(self):
+        return self.title
--- boards/views.py
+++ boards/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .models import Board
+from .serializers import BoardSerializer
+from django.db import connection
 
-# Create your views here.
+class BoardMainView(generics.GenericAPIView):
+    serializer_class = BoardSerializer
+    
+    def get(self, request, PageId):
+        try:
+            board = Board.objects.get(pageId=PageId)
+        except Board.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM boards_board WHERE pageId = %s", [PageId])
+            row = cursor.fetchone()
+        if row is None:
+            return Response (status=status.HTTP_404_NOT_FOUND)
+        else:
+            return Response(row, status=status.HTTP_200_OK)
+            
