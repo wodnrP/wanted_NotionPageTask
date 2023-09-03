a7453f1be4655bb944f673b12b4a77177a6e3721
ramyo564 <yohan032yohan@gmail.com>
2023-09-03 12:03:38 +0900

    feat: admin 구성
    어드민 페이지 구성

--- pages/admin.py
+++ pages/admin.py
@@ -1,1 +1,33 @@
 from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline
+from .models import (
+    Subject_A, Subject_B, Subject_C, Subject_D, Page,
+)
+
+
+class PageInline(GenericTabularInline):
+    model = Page
+    extra = 1
+
+
+@admin.register(Subject_A)
+class SubjectAAdmin(admin.ModelAdmin):
+    inlines = [PageInline]
+
+
+@admin.register(Subject_B)
+class SubjectBAdmin(admin.ModelAdmin):
+    inlines = [PageInline]
+
+
+@admin.register(Subject_C)
+class SubjectCAdmin(admin.ModelAdmin):
+    inlines = [PageInline]
+
+
+@admin.register(Subject_D)
+class SubjectDAdmin(admin.ModelAdmin):
+    inlines = [PageInline]
+
+
+admin.site.register(Page)
